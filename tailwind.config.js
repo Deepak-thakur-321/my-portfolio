f62340855636080b801b959876087203851022cd
DeepakThakur <deeepakthakur17756@gmail.com>
2025-04-10 10:31:16 +0530

    change some tailwind path and add output.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./script.jsx"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
-}
-
+};
